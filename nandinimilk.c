f6ae92ab5022d44002c31d372ee0779570fbae99
295Haripriya <120159097+295Haripriya@users.noreply.github.com>
2024-03-11 09:42:15 -0700

    Add files via upload

---
+++ nandinimilk.c
@@ -0,0 +1,17 @@
+// modular c program to compute the total bill for beverage
+/*
+Name:Apoorva
+Roll no.:1910
+Division:S
+Date:16/01/2023
+*/
+#include<stdio.h>
+// function prototype
+float total_bill_amount(char type,int quantity);
+int main()
+{
+    char type;
+    int quantity;
+    float total_bill;
+    printf("Enter type of beverage(in uppe")
+}
