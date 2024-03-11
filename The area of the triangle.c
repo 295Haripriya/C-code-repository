dc33800f2baf7f9603d4a6e10e0e09fceea652c6
295Haripriya <120159097+295Haripriya@users.noreply.github.com>
2024-03-11 09:39:36 -0700

    Add files via upload

---
+++ The area of the triangle.c
@@ -0,0 +1,22 @@
+// modular c program to compute area of triangle
+#include<stdio.h>
+// function prototype
+float area(float b,float h);
+// main function
+int main()
+{
+    float b,h,areal;
+    printf("Enter the base and height of the triangle\n");
+    scanf("%f%f",&b,&h);
+    // function call
+    areal=area(b,h);
+    printf("area is %0.2f\n",areal);
+    return 0;
+}//end of main
+// function definition
+float area(float b,float h)
+{
+    float area1;
+    area1=(0.5)*b*h;
+    return area1;
+}
